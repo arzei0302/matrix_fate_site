1a7c5adb308f503d785987ea8321a71f4aa996ef
arzei0302 <kabulov.arz@gmail.com>
2025-04-20 11:01:17 +0600

    permission

--- matrix_fate/common/permissions.py
+++ matrix_fate/common/permissions.py
@@ -1,19 +1,3 @@
-# from rest_framework.permissions import BasePermission
-
-# class IsLevel3OrHigher(BasePermission):
-#     """
-#     Разрешает доступ только пользователям с уровнем доступа 'level2', 'level3' или 'level4'.
-#     """
-#     def has_permission(self, request, view):
-#         user = request.user
-#         if not user.is_authenticated:
-#             return False
-#         profile = getattr(user, 'profile', None)
-#         if profile and profile.access_level in ['level2', 'level3', 'level4']:
-#             return True
-#         return False
-
-
 from rest_framework.permissions import BasePermission
 from django.utils import timezone
 
@@ -37,11 +21,9 @@ class IsActivePaidUser(BasePermission):
         if not profile:
             return False
 
-        # Проверка уровня доступа
         if profile.access_level not in self.allowed_levels:
             return False
 
-        # Проверка, что дата окончания доступа установлена и не истекла
         if profile.access_expiration is None:
             return False
 
